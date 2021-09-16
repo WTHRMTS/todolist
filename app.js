a21a4571e83bd8fd3944fbec5a128e154c227288
Jamie Booth <Malediction99@gmail.com>
2021-09-16 17:48:58 +1000

    Just need add button fix

--- app.js
+++ app.js
@@ -1,42 +1,27 @@
 const { urlencoded } = require("express");
 const express =  require("express");
+const { getDefaultSettings } = require("http2");
 const https = require("https");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 app.use(urlencoded({extended: true}));
-
 
 app.set('view engine', 'ejs');
 
 app.use(express.static("public"));
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
-let workItems = ["Check Email", "Log Hours"];
-let numItems = items.length;
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = ["Check Email", "Log Hours"];
+
 app.get("/", (req, res) => {
-
-    let today = new Date();
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    let day = today.toLocaleDateString("en-AU", options);
-    // let daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    // let day = daysOfTheWeek[currentDay];
-    // if (currentDay < 0 || currentDay > 6){
-    //     console.log("Error: current day is equal to: " + currentDay);
-    // };
-    res.render("list", {
-        listTitle: day,
-        numItems: numItems,
-        newListItems: items
-    });
+    
+    const day = date.getDay();
+    res.render("list", { listTitle: day, newListItems: items });
 });
 
 app.post("/", (req, res) => {
-    let item = req.body.newItem;
+    const item = req.body.newItem;
     if (req.body.list === "Work") {
         workItems.push(item);
         res.redirect("/work");
@@ -53,12 +38,6 @@ app.get("/work", (req, res) => {
 app.get("/about", (req, res) => {
     res.render("about");
 })
-// app.post("/work", (req, res) => {
-//     let item = req.body.newItem;
-//     workItems.push(item);
-//     console.log(workItems);
-//     res.redirect("/work");
-// });
 
 app.listen(3000, () => {
     console.log("Server is now active on Port 3000");
